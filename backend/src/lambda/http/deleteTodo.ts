c58569a9a32b9d16bd77c61689d3b014948765ad
Codart-7 <iconmoa@gmail.com>
2022-10-07 01:25:48 +0100

    [update] screenshots

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,38 +1,47 @@
 import 'source-map-support/register'
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { deleteToDoItem } from '../../helpers/BusinessLogic/todos'
 import { createLogger } from '../../utils/logger';
 import { parseUserId } from '../../auth/utils'
 import { getToken } from '../../auth/utils'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
 
 const logger = createLogger('deleteTodo');
 
-export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  logger.info('Processing DeleteTodo event...');
-  const jwtToken: string = getToken(event);
-  const userId = parseUserId(jwtToken);
-  const todoId = event.pathParameters.todoId;
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
-  };
 
-  try {
-    const deleteItem = await deleteToDoItem(userId, todoId)
-    logger.info(`Successfully deleted todo item: ${todoId}`);
-    return {
-      statusCode: 204,
-      headers,
-      body: JSON.stringify(deleteItem)
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Processing DeleteTodo event...');
+    const jwtToken: string = getToken(event);
+    const userId = parseUserId(jwtToken);
+    const todoId = event.pathParameters.todoId;
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
     };
-  } catch (error) {
-    logger.error(`Error: ${error.message}`);
-    return {
-      statusCode: 500,
-      headers,
-      body: JSON.stringify({ error })
-    };
+
+    try {
+      const deleteItem = await deleteToDoItem(userId, todoId)
+      logger.info(`Successfully deleted todo item: ${todoId}`);
+      return {
+        statusCode: 204,
+        headers,
+        body: JSON.stringify(deleteItem)
+      };
+    } catch (error) {
+      logger.error(`Error: ${error.message}`);
+      return {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({ error })
+      };
+    }
   }
-};
+);
+
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true
+  })
+)
